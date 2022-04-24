7c7e74348f437667b7fd90dd954a580ea0a1a37c
Jacek Kitlinski <gasniczka@o2.pl>
2022-04-24 15:24:57 +0200

    nowy koment 2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,9 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("test do maven jenkins");
-        // koment w celu weryfikacji dzialania jeninsa
+        // koment 1 w celu weryfikacji dzialania jeninsa
+        // koment 2 w celu weryfikacji dzialania jeninsa
+
     }
 
 
